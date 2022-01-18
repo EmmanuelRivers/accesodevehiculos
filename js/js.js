71fbbec5eb47f25b95d2b6f04134a263504f7b34
EmmanuelRivers <97993499+EmmanuelRivers@users.noreply.github.com>
2022-01-18 17:18:35 -0600

    JavaScrip
    Archivo con métodos js

---
+++ js/js.js
@@ -0,0 +1,11 @@
+function irrentrada() {
+	document.location='http://localhost/accesodevehiculos/registroEntrada.php';
+}
+
+function irrsalida() {
+	document.location='http://localhost/accesodevehiculos/registroSalida.php';
+}
+
+function irinforme() {
+	document.location='http://localhost/accesodevehiculos/informe.php';
+}
